91adc513d238d10fe3ed014b5b11f8bda203c459
SunHo Van <vsh123@naver.com>
2019-09-10 14:34:58 +0900

    initial commit

---
+++ src/main/java/van/VanApplication.java
@@ -0,0 +1,11 @@
+package van;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VanApplication.class, args);
+    }
+}
